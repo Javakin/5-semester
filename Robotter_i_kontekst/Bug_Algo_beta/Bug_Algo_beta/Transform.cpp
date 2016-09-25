cb448a8a117a6824179bcaa189113e60b3f93d81
Korbu <000korbu000@gmail.com>
2016-09-25 16:33:18 +0200

    Transform lagt i h o cpp fil

--- Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Transform.cpp
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Transform.cpp
@@ -2,11 +2,27 @@
 
 
 
-Transform::Transform()
+
+int main(int argc, char** argv)
 {
-}
+	std::cout << "Hello" << std::endl;
+
+	Transform transform1(1, 0, 0);
+	Transform transform2(0, 0, 45 * PI / 180.0);
+	Transform transform3(1, 0, 0);
+	Transform transform4(0, 0, 45 * PI / 180.0);
+
+	std::cout << "Transform1:";
+	for (size_t i = 0; i < 3; i++) {
+		for (size_t j = 0; j < 3; j++) {
+			std::cout << transform1(i, j) << "  ";
+		}
+		std::cout << std::endl;
+	}
 
 
-Transform::~Transform()
-{
+	Point p(0, 0);
+	Point p2 = transform1.mult(transform2.mult(transform3.mult(transform4.mult(p))));
+	std::cout << "Transformed Point " << p2(0) << ", " << p2(1) << ", " << p2(2) << std::endl;
+	return 0;
 }
