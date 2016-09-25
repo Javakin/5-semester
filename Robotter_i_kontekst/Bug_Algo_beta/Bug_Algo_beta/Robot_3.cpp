46295a07b74861f21f485de1fe4d60ffb2acf713
Javakin <danielharaldson@yahoo.com>
2016-09-25 15:47:29 +0200

    f

--- Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Robot_3.cpp
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Robot_3.cpp
@@ -22,12 +22,12 @@ void Robot_3::printMotionSequence(Point, Transform)
 	int sec;
 	int x;
 	int y;
-
+	
 	// Tabel of the movement for the two wheels and the time they move
 	//std::cout << "| Wheel 1 " << "| Wheel 2 " << "| Time [sec] |" << std::endl;
 	//std::cout << "Step 1 | " << x << " | " << y << " | " << sec << " | " << std::endl;
 	//std::cout << "Step 2 | " << x << " | " << y << " | " << sec << " | " << std::endl;
-	//std::cout << "Step 3 | " << x << " | " << y << " | " << sec << " | " << std::endl;
+	//std::cout << "Step 3 | " << x << " | " << y << " | " << sec << " | " << std::endl; 
 }
 
 void Robot_3::traceRoute(Transform motion)
