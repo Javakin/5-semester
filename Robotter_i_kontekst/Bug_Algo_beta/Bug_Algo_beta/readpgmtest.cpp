6ac50eded6e6c55e4ec7953df162cf50e4585831
Javakin <danielharaldson@yahoo.com>
2016-09-25 09:45:59 +0200

    added bug_Algo_beta

---
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/readpgmtest.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "Image.h"
+#include "PPMLoader.h"
+
+using namespace rw::sensor;
+using namespace rw::loaders;
+
+int main(int argc, char** argv) {
+	std::string filename(argv[1]);
+	std::cout << filename << std::endl;
+
+
+	std::cout << "loading image..." << std::endl;
+	Image* img = PPMLoader::load(filename);
+
+	// do stuff here
+	int channel = 0; // allways 0 on grayscale image
+
+	std::cout << "inverting image..." << std::endl;
+	// change all 0 pixels to 255 and all 255 pixels to 0
+	for (int x = 0; x<img->getWidth(); x++) {
+		for (int y = 0; y<img->getHeight(); y++) {
+			int val = img->getPixelValuei(x, y, channel);
+			if (val == 0) {
+				img->setPixel8U(x, y, 255);
+			}
+			else if (val == 255) {
+				img->setPixel8U(x, y, 0);
+			}
+		}
+	}
+
+	std::cout << "saving image..." << std::endl;
+	// save image HELT UDE I ROOT MAPPEN OMG!!!	
+	img->saveAsPGM("testout.pgm");
+
+	// cleanup
+	delete img;
+
+	// test til debugging
+
+	//std::cout << argv[1] << std::endl;
+
+	// final statements
+	system("pause");
+	return 0;
+}
