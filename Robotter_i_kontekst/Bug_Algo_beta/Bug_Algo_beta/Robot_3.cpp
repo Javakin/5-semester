2d3b503d80fefdd5c178ea02a576d389cbd43b8c
Javakin <danielharaldson@yahoo.com>
2016-09-25 15:40:44 +0200

    opdateret robot_3

--- Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Robot_3.cpp
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Robot_3.cpp
@@ -12,10 +12,30 @@ Robot_3::~Robot_3()
 {
 }
 
-void Robot_3::printMotionSequence(point, Transform)
+void Robot_3::goTo(double x, double y, double theta)
 {
-	std::cout <<		"| Wheel 1 " << "| Wheel 2 " << "| Wheel 3|" << std::endl;
-	std::cout << "Step 1 | " << x << " | " << y << " | " << o << " | " << endl;
-	std::cout << "Step 2 | " << x << " | " << y << " | " << o << " | " << endl;
-	std::cout << "Step 3 | " << x << " | " << y << " | " << o << " | " << endl;
+
+}
+
+void Robot_3::printMotionSequence(Point, Transform)
+{
+	int sec;
+	int x;
+	int y;
+
+	// Tabel of the movement for the two wheels and the time they move
+	//std::cout << "| Wheel 1 " << "| Wheel 2 " << "| Time [sec] |" << std::endl;
+	//std::cout << "Step 1 | " << x << " | " << y << " | " << sec << " | " << std::endl;
+	//std::cout << "Step 2 | " << x << " | " << y << " | " << sec << " | " << std::endl;
+	//std::cout << "Step 3 | " << x << " | " << y << " | " << sec << " | " << std::endl;
+}
+
+void Robot_3::traceRoute(Transform motion)
+{
+
+}
+
+int Robot_3::calcualteWorkSpace(Transform motion)
+{
+	return 0;
 }
