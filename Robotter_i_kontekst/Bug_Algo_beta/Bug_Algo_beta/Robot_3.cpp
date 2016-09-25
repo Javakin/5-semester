7d22d118913e8459a79539d8e5e6e900f384e826
Korbu <000korbu000@gmail.com>
2016-09-25 14:31:58 +0200

    pass

--- Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Robot_3.cpp
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Robot_3.cpp
@@ -1,21 +1,21 @@
-#include "Robot_3.h"
-
-
-
-Robot_3::Robot_3()
-{
-}
-
-
-
-Robot_3::~Robot_3()
-{
-}
-
-void Robot_3::printMotionSequence(point, Transform)
-{
-	std::cout <<		"| Wheel 1 " << "| Wheel 2 " << "| Wheel 3|" << std::endl;
-	std::cout << "Step 1 | " << x << " | " << y << " | " << o << " | " << endl;
-	std::cout << "Step 2 | " << x << " | " << y << " | " << o << " | " << endl;
-	std::cout << "Step 3 | " << x << " | " << y << " | " << o << " | " << endl;
-}
+//#include "Robot_3.h"
+//
+//
+//
+//Robot_3::Robot_3()
+//{
+//}
+//
+//
+//
+//Robot_3::~Robot_3()
+//{
+//}
+//
+//void Robot_3::printMotionSequence(point, Transform)
+//{
+//	std::cout <<		"| Wheel 1 " << "| Wheel 2 " << "| Wheel 3|" << std::endl;
+//	std::cout << "Step 1 | " << x << " | " << y << " | " << o << " | " << endl;
+//	std::cout << "Step 2 | " << x << " | " << y << " | " << o << " | " << endl;
+//	std::cout << "Step 3 | " << x << " | " << y << " | " << o << " | " << endl;
+//}
