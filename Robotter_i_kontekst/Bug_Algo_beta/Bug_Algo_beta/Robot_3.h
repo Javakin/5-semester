0139df83ccc336a0547c6a3265300692cf8e69eb
Korbu <000korbu000@gmail.com>
2016-09-25 14:25:51 +0200

    Start po Robot

---
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Robot_3.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+
+class Robot_3
+{
+public:
+	Robot_3();
+
+
+
+	Robot_3(double, double, double);
+
+	void goTo(double, double, double);
+
+	void printMotionSequence(point, Transform);
+
+	~Robot_3();
+
+	point current_point;
+
+	image map;
+
+	
+};
+
