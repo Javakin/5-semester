051a8a986d4cacfe5512ea3858bccd18f79ef6d2
Korbu <000korbu000@gmail.com>
2016-09-25 15:13:22 +0200

    Pseudo code for Robot_3

--- Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Robot_3.h
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/Robot_3.h
@@ -1,26 +1,39 @@
-//#pragma once
-//
-//#include <iostream>
-//
-//class Robot_3
-//{
-//public:
-//	Robot_3();
-//
-//
-//
-//	Robot_3(double, double, double);
-//
-//	void goTo(double, double, double);
-//
-//	void printMotionSequence(point, Transform);
-//
-//	~Robot_3();
-//
-//	point current_point;
-//
-//	image map;
-//
-//	
-//};
-//
+#pragma once
+
+#include "Point.h"
+#include "Image.h"
+#include "Transform.h"
+
+#include <iostream>
+
+class Robot_3
+{
+public:
+	Robot_3();
+
+	// Construct a robot at the position x,y and with the facing theta
+	Robot_3(double x, double y, double theta);
+
+	// Move robot to new position
+	void goTo(double x, double y, double theta);
+
+	// Print out the movement from the input transformation
+	void printMotionSequence(Point position, Transform motion);
+
+	// Trace the movment of the robot on the map(image)
+	void traceRoute(Transform motion);
+
+	// Calcualte the requierd work space
+	int calcualteWorkSpace(Transform motion);
+
+
+
+	~Robot_3();
+private:
+	Point current_point;
+
+	rw::sensor::Image* map;
+
+	
+};
+
