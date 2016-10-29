47ce75219fc0cad2fe820e84ac1f80459ee7586f
Korbu <000korbu000@gmail.com>
2016-10-29 19:09:29 +0200

    Lidt mere

--- ADA/Balls_and_bins/Balls_and_bins/Balls_and_bins/Source.cpp
+++ ADA/Balls_and_bins/Balls_and_bins/Balls_and_bins/Source.cpp
@@ -4,13 +4,27 @@
 
 #include <math.h>
 #include <iostream>
-
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+	
+
 	std::cout << "log(log(100007)) = " << log(log(100007)) << std::endl;
 
 	std::cout << "log(100007) = " << log(100007) << std::endl;
 
+	int bins[11] = {0,0,0,0,0,0,0,0,0,0,0};
+
+	std::cout << bins[1] << std::endl;
+
+	srand(time(NULL));
+	int x = rand() % 10;
+	std::cout << x << std::endl;
+
+
+
 	return 0;
+
 }
