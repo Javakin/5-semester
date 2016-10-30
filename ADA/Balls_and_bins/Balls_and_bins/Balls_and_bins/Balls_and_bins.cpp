ef08c7a6d82e19b72b7a0cc65348bf0e94765c9f
Javakin <danielharaldson@yahoo.com>
2016-10-30 15:39:03 +0100

    så er der lavet en constructor til balls and bins

--- ADA/Balls_and_bins/Balls_and_bins/Balls_and_bins/Balls_and_bins.cpp
+++ ADA/Balls_and_bins/Balls_and_bins/Balls_and_bins/Balls_and_bins.cpp
@@ -1,9 +1,22 @@
 #include "Balls_and_bins.h"
 
 
-
 Balls_and_bins::Balls_and_bins()
 {
+	// setup the binvector
+	bin_vector.clear();
+	bin_vector.resize(DEFOULT_VECTOR_SIZE, 0);
+
+	cout << "size: " << bin_vector.size() << endl;
+}
+
+Balls_and_bins::Balls_and_bins(int bins)
+{
+	// setup the bin vector
+	bin_vector.clear();
+	bin_vector.resize(bins, 0);
+	
+	cout << "size: " << bin_vector.size() << endl;
 }
 
 
--- ADA/Balls_and_bins/Balls_and_bins/Balls_and_bins/Balls_and_bins.h
+++ ADA/Balls_and_bins/Balls_and_bins/Balls_and_bins/Balls_and_bins.h
@@ -1,8 +1,21 @@
 #pragma once
+#include <vector>
+#include <iostream>
+
+#define DEFOULT_VECTOR_SIZE		1000000
+
+using namespace std;
+
 class Balls_and_bins
 {
 public:
 	Balls_and_bins();
-	~Balls_and_bins();
-};
+	Balls_and_bins(int bins);
 
+
+	~Balls_and_bins();
+
+private:
+	vector<int> bin_vector;
+
+};
