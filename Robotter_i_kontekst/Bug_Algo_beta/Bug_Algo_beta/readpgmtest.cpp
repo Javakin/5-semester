650045fc94911e9a4290625948319816aa9e9711
Korbu <000korbu000@gmail.com>
2016-11-08 16:18:08 +0100

    Lidt endringer

--- Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/readpgmtest.cpp
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/readpgmtest.cpp
@@ -21,31 +21,15 @@ int main(int argc, char** argv) {
 	std::cout << "loading image..." << std::endl;
 	Image* img = PPMLoader::load(filename);
 
-	// initiate
-	//Robot wall_e(img);
-	//Transform H4(200, 500, 3);
-	
-	/*Mapping invert;
-	invert.diagram();*/
-	// This part is going inside "Mapping.cpp"
 
-	cout << "Width of image: " << img->getWidth() << endl;
-	cout << "Height of image: " << img->getHeight() << endl;
+	//cout << "Width of image: " << img->getWidth() << endl;
+	//cout << "Height of image: " << img->getHeight() << endl;
 	int rows = img->getHeight();
 	int cols = img->getWidth();
 	int val = 1;
 	int x = 0;
 	int y = 0;
-	int xx = 0;
-	int yy = 0;
-	int xxx = 0;
-	int yyy = img->getHeight();
-	int xxxx = img->getHeight();
-	int yyyy = img->getHeight();
-	int xxxxx = img->getHeight();
-	int yyyyy = 0;
 
-	int testIfblack = 0;
 	vector<vector<int>> testvec(rows,vector<int> (cols, val));
 	int channel = 0; // allways 0 on grayscale image
 
@@ -69,37 +53,7 @@ int main(int argc, char** argv) {
 			}
 		}
 	}
-	for (int i = 0; i < img->getWidth()-1; i++)
-	{
-			if (testvec[xx + 1][yy + 1] != 254 && testvec[xx + 1][yy] && testvec[xx][yy + 1])
-			{
-				++xx;
-				++yy;
-				testvec[xx][yy] = 254;
-			}
 
-			if (testvec[xxx + 1][yyy - 1] != 254 && testvec[xxx + 1][yyy - 2] != 254 && testvec[xxx][yyy - 1] != 254)
-			{
-				++xxx;
-				--yyy;
-				testvec[xxx][yyy] = 254;
-			}
-			
-			if (testvec[xxxx - 1][yyyy - 1] != 254 && testvec[xxxx - 1][yyyy - 2] != 254 && testvec[xxxx - 2][yyyy - 1] != 254)
-			{
-				--xxxx;
-				--yyyy;
-				testvec[xxxx][yyyy] = 254;
-			}
-			/*&& testvec[xxxxx - 1][yyyyy - 2] != 254 && testvec[xxxxx - 2][yyyyy - 1] != 254*/
-			if (testvec[xxxxx - 1][yyyyy + 1] != 254 && testvec[xxxxx - 1][yyyyy] != 254 /*&& testvec[xxxxx][yyyyy + 1] != 254*/)
-			{
-				--xxxxx;
-				++yyyyy;
-				testvec[xxxxx][yyyyy] = 254;
-			}
-	}
-	testvec[xxx + 1][yyy - 1] = 254;
 	for (unsigned int x = 0; x < img->getWidth(); ++x) {
 		for (unsigned int y = 0; y < img->getHeight(); ++y) {
 			if (testvec[x][y] == 254) {
@@ -108,35 +62,9 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	//for (int i = 1; i < 10; i++)
-	//{
-	//	++xx;
-	//	++yy;
-	//	testvec[xx][yy] = 254;
-	//}
-	/*for (auto i : testvec)
-		cout << "Test vec val: " << i << " " << endl;*/
-	/*for (auto i = testvec.begin(); i < testvec.end(); ++i)
-	{
-		cout << "Test vec val: " << testvec[i] << " i: " << i << endl;
-	}*/
-
 	std::cout << "saving image..." << std::endl;
 	// save image
 	img->saveAsPGM("testout.pgm");
-
-	
-
-	/*Mapping testtt;
-	testtt.test();*/
-
-	//std::cout << "Test off getWidth(): " << img->getWidth() << std::endl;
-
-	// printing image
-	//std::cout << "saving image..." << std::endl;
-	//img->saveAsPGM("testout.pgm");
-	
-
 
 	// cleanup
 	delete img;
