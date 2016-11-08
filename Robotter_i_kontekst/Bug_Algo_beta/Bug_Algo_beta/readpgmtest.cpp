822febc50dd3f653585be6ca7de36437af802348
Steen-Bjørn <000korbu000@gmail.com>
2016-11-08 02:07:35 +0100

    Ser ud til at jeg kan tegne streger nu. Nu med hjørner,

--- Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/readpgmtest.cpp
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/readpgmtest.cpp
@@ -36,6 +36,16 @@ int main(int argc, char** argv) {
 	int val = 1;
 	int x = 0;
 	int y = 0;
+	int xx = 0;
+	int yy = 0;
+	int xxx = 0;
+	int yyy = img->getHeight();
+	int xxxx = img->getHeight();
+	int yyyy = img->getHeight();
+	int xxxxx = img->getHeight();
+	int yyyyy = 0;
+
+	int testIfblack = 0;
 	vector<vector<int>> testvec(rows,vector<int> (cols, val));
 	int channel = 0; // allways 0 on grayscale image
 
@@ -48,72 +58,6 @@ int main(int argc, char** argv) {
 
 			if (x < img->getWidth()-1 && x > 1 && y < img->getHeight()-1 && y > 1)
 			{
-				/*if (testvec[x - 1][y] == testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x - 1][y - 1] == testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x - 1][y + 1] == testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x + 1][y] == testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x + 1][y - 1] == testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x + 1][y + 1] == testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x][y - 1] == testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x][y + 1] == testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}*/
-
-				/*if (testvec[x - 1][y] != testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x - 1][y - 1] != testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x - 1][y + 1] != testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x + 1][y] != testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x + 1][y - 1] != testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x + 1][y + 1] != testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x][y - 1] != testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}
-				else if (testvec[x][y + 1] != testvec[x][y] && testvec[x][y] != 255)
-				{
-					testvec[x][y] = 255;
-				}*/
-				
 				if (testvec[x][y] != testvec[x - 1 ][y] && testvec[x][y] != testvec[x + 1 ][y] && testvec[x - 1][y] != 254 && testvec[x + 1][y] != 254)
 				{
 					testvec[x][y] = 254;
@@ -123,19 +67,53 @@ int main(int argc, char** argv) {
 					testvec[x][y] = 254;
 				}
 			}
-				/*if (testvec[x][y] == 255)
-				{
-					cout << " Testvec pos with val 255: " << x << ", " << y << endl;
-				}*/
-			
-			if (testvec[x][y] == 254) {
-				img->setPixel8U(x, y, 100);
-			}
-			/*else if (val < 127) {
-				img->setPixel8U(x, y, 0);
-			}*/
 		}
 	}
+	for (int i = 0; i < img->getWidth()-1; i++)
+	{
+			if (testvec[xx + 1][yy + 1] != 254 && testvec[xx + 1][yy] && testvec[xx][yy + 1])
+			{
+				++xx;
+				++yy;
+				testvec[xx][yy] = 254;
+			}
+
+			if (testvec[xxx + 1][yyy - 1] != 254 && testvec[xxx + 1][yyy - 2] != 254 && testvec[xxx][yyy - 1] != 254)
+			{
+				++xxx;
+				--yyy;
+				testvec[xxx][yyy] = 254;
+			}
+			
+			if (testvec[xxxx - 1][yyyy - 1] != 254 && testvec[xxxx - 1][yyyy - 2] != 254 && testvec[xxxx - 2][yyyy - 1] != 254)
+			{
+				--xxxx;
+				--yyyy;
+				testvec[xxxx][yyyy] = 254;
+			}
+			/*&& testvec[xxxxx - 1][yyyyy - 2] != 254 && testvec[xxxxx - 2][yyyyy - 1] != 254*/
+			if (testvec[xxxxx - 1][yyyyy + 1] != 254 && testvec[xxxxx - 1][yyyyy] != 254 /*&& testvec[xxxxx][yyyyy + 1] != 254*/)
+			{
+				--xxxxx;
+				++yyyyy;
+				testvec[xxxxx][yyyyy] = 254;
+			}
+	}
+	testvec[xxx + 1][yyy - 1] = 254;
+	for (unsigned int x = 0; x < img->getWidth(); ++x) {
+		for (unsigned int y = 0; y < img->getHeight(); ++y) {
+			if (testvec[x][y] == 254) {
+				img->setPixel8U(x, y, 0);
+			}
+		}
+	}
+
+	//for (int i = 1; i < 10; i++)
+	//{
+	//	++xx;
+	//	++yy;
+	//	testvec[xx][yy] = 254;
+	//}
 	/*for (auto i : testvec)
 		cout << "Test vec val: " << i << " " << endl;*/
 	/*for (auto i = testvec.begin(); i < testvec.end(); ++i)
