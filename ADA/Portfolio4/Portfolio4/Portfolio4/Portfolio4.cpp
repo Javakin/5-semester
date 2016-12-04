e1103284a49f3998937d9c6563ede11c4e6455a6
Javakin <danielharaldson@yahoo.com>
2016-12-04 16:35:41 +0100

    lavet program

---
+++ ADA/Portfolio4/Portfolio4/Portfolio4/Portfolio4.cpp
@@ -0,0 +1,18 @@
+// Portfolio4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	// for detection of memory leaks 
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+
+
+	system("pause");
+    return 0;
+}
+
