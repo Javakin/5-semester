e0569a57ca491905c652925a02c5f99669a92c55
Javakin <danielharaldson@yahoo.com>
2016-12-06 11:30:09 +0100

    dijkstra virker vist nu

--- ADA/Portfolio4/Portfolio4/Portfolio4/Portfolio4.cpp
+++ ADA/Portfolio4/Portfolio4/Portfolio4/Portfolio4.cpp
@@ -17,17 +17,19 @@ int main()
 	// Implement a graph
 	///////////////////////////////////////////////////////////////
 
-	Graph aGraph(10);
+	Graph aGraph(4);
+	aGraph.insertEdge(1, 2);
+	aGraph.insertEdge(4, 2);
+	aGraph.insertEdge(3, 4);
+
+
+	/*
 	aGraph.insertEdge(1, 9);
 	aGraph.insertEdge(9, 8);
-	aGraph.insertEdge(7, 1); 
-	aGraph.insertEdge(5, 2);
-	aGraph.insertEdge(2, 6);
-	aGraph.insertEdge(5, 1);
 	aGraph.insertEdge(2, 5, 2);
 	aGraph.insertEdge(9, 7, 3);
-
-	aGraph.deleteEdge(1, 9);
+	*/
+	//aGraph.deleteEdge(1, 9);
 
 	aGraph.printGraph("Printed graph");
 
@@ -39,20 +41,31 @@ int main()
 
 	cout << "\nPrint Topological sort:\n";
 	for (unsigned int i : sortedList)
-		cout << i << ", "; 
+		cout << i + 1 << ", "; 
 	cout << endl;
 
 	///////////////////////////////////////////////////////////////
 	// do dijkstra
 	///////////////////////////////////////////////////////////////
+
+	Graph bGraph(10);
+	bGraph.insertEdge(1, 2, 2);
+	bGraph.insertEdge(2, 3, 7);
+	bGraph.insertEdge(3, 4, 5);
+	bGraph.insertEdge(4, 7, 1);
+	bGraph.insertEdge(4, 5, 10);
+
+	bGraph.printGraph("Printed graph 2");
+
 	vector<weighting> pathTree;
 	vector<unsigned int> path;
-	pathTree = aGraph.dijkstra(1);
-	path = aGraph.dijkstra(1, 8);
+	pathTree = bGraph.dijkstra(0);
+	path = bGraph.dijkstra(0, 5);
 
 	cout << "\nPrint dijkstra pathTree:\n";
-	for (weighting w : pathTree)
-		cout << w.parrentVertex << "\t" << w.vertexWeighting << endl;
+	cout << "vertex | parrent | weight\n";
+	for (unsigned int i = 0; i< pathTree.size(); i++)
+		cout << i + 1 << "\t" <<  pathTree[i].parrentVertex +1 << "\t" << pathTree[i].vertexWeighting << endl;
 	cout << endl;
 
 	cout << "\nPrint dijkstra path:\n";
