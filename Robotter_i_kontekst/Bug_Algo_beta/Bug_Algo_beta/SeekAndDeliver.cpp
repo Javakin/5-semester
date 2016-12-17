b35f67cdb1b9570dd591cd50943bb46d1d15ff3e
Javakin <danielharaldson@yahoo.com>
2016-12-17 12:43:28 +0100

    commit goddamit

---
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/SeekAndDeliver.cpp
@@ -0,0 +1,26 @@
+#include "SeekAndDeliver.h"
+
+
+
+SeekAndDeliver::SeekAndDeliver()
+{
+}
+
+
+SeekAndDeliver::~SeekAndDeliver()
+{
+}
+
+void SeekAndDeliver::coverragePlaning()
+{
+}
+
+bool SeekAndDeliver::deliverCoin(unsigned int xVal, unsigned int yVal)
+{
+	return false;
+}
+
+bool SeekAndDeliver::hasConpleted()
+{
+	return false;
+}
---
+++ Robotter_i_kontekst/Bug_Algo_beta/Bug_Algo_beta/SeekAndDeliver.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+class SeekAndDeliver
+{
+public:
+	SeekAndDeliver();
+	~SeekAndDeliver();
+
+	void coverragePlaning();
+
+
+private: 
+	bool deliverCoin(unsigned int xVal, unsigned int yVal);
+	bool hasConpleted();
+
+};
+
