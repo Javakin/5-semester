95c2919dcf564f9286b52717629e3000c774304f
Javakin <danielharaldson@yahoo.com>
2016-12-29 14:41:06 +0100

    docker is now installing mysql but fails to setup the database

--- leo/portfolios/portfolio_2/DB/db_setup.sh
+++ leo/portfolios/portfolio_2/DB/db_setup.sh
@@ -8,10 +8,10 @@ imageName="db"
 containerName="db_container"
 
 # remove old container
-#echo "Stopping database container: $containerName"
-#docker stop $containerName
-#echo "Removing database container: $containerName"
-#docker rm $containerName
+echo "Stopping database container: $containerName"
+docker stop $containerName
+echo "Removing database container: $containerName"
+docker rm $containerName
 
 # remove old image
 #docker rmi imageName
@@ -23,3 +23,4 @@ docker build -t $imageName .
 
 # run container 
 #docker run -dit --name $containerName $imageName
+#docker run -dit --name db_container db
